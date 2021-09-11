17bd00c9e0aa6644a53f1c37303238bc9a48f1b5
IS-Rachinthana <isavindurachintha@gmail.com>
2021-09-11 21:40:39 +0530

    File Uploading Php part

--- Suggestions/profile.php
+++ Suggestions/profile.php
@@ -1,1 +1,15 @@
-<html>
+<?php
+
+if(isset($_FILES["myfile1"]["name"])){
+
+$fname=$_FILES["myfile1"]["name"];
+  $ftemp=$_FILES["myfile1"]["tmp_name"];
+  $flag=move_uploaded_file($ftemp,"img/$fname");
+  if($flag)
+  {echo "File Uplaod Successfully!";
+    echo"$flag";}
+  else
+  {echo " Sorry couldn't move";}
+
+}
+?>
