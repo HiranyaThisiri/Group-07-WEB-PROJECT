48169e22251c3586a543547b7cd35839d47474a4
IS-Rachinthana <isavindurachintha@gmail.com>
2021-09-11 21:27:49 +0530

    Login page PHP part

--- Suggestions/login.php
+++ Suggestions/login.php
@@ -1,1 +1,46 @@
-<html>
+<?php
+if(isset($_POST['email'])&&isset($_POST['password'])){
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+
+// database connection here
+$con = new mysqli("localhost", "root", "","save_aqua_db");
+if ($con-> connect_error)
+ {
+  die("Failed to connect: ".$con->connect_error);
+}
+
+else
+{
+  $stmt = $con->prepare("select * from registration where email = ?");
+  $stmt -> bind_param("s",$email);
+  $stmt->execute(); 
+  $stmt_result = $stmt->get_result();
+  if ($stmt_result->num_rows > 0) 
+  {
+    $data = $stmt_result->fetch_assoc();
+    $data['password']=str_replace(' ','',$data['password']);
+
+
+    if($data['password']== $password)
+    {
+      echo "<h2>Log in Successfully</h2>";
+      //window.location.replace="profile.php";
+      //action="profile.php";
+      $link="save_aqua_temp/profile.php";
+      header('location:profile.php');
+    }
+    else
+    {
+      echo "<h2>Invalid Email or Password</h2>";
+    }
+  }
+  else
+  {
+    echo "<h2> Invalid Email or password </h2>";
+  }
+
+}
+}
+?>
